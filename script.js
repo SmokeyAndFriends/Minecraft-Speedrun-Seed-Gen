22edb0fee94011f3747e556f84882671a57fecd3
Ethan Belcher <65253767+SmokeyAndFriends@users.noreply.github.com>
2021-04-08 13:29:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const msbtn = document.getElementById('msbtn');
+const input = document.getElementById('input');
+const label = document.getElementById('label');
 
+let seed = undefined;
+
+
+
+msbtn.addEventListener("click", function() {
+  seed += math.pow(2, 48);
+  label.textContent = `Your new seed is: ${seed}`
+});
